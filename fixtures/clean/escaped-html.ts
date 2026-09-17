04dd53d1917107ae3dfb198453d65d1c235a140d
dtsuka <dtsuka@gmail.com>
2026-09-16 19:03:00 -0700

    Add escaped HTML clean fixture

---
+++ fixtures/clean/escaped-html.ts
@@ -0,0 +1,4 @@
+export function escapeHtml(value: string) {
+  return value.replace(/[&<>"']/g, (c) => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' }[c]!));
+}
+export function renderComment(comment: string) { return `<div>${escapeHtml(comment)}</div>`; }
